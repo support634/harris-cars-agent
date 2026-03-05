0c949697e950836d62c53b3f19d12a5ad3db33da
rdtungul <rosephdarl@gmail.com>
2026-03-06 05:51:20 +0800

    Fix build issues + vite config

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,33 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/js/app.js'
             ],
-            refresh: true,
-        }),
+            refresh: true
+        })
     ],
+
     server: {
-        host: '0.0.0.0',    // REQUIRED inside Docker — allows container access
+        host: true,
         port: 5173,
         hmr: {
-            host: 'localhost',
-        },
+            host: '0.0.0.0'
+        }
     },
+
     build: {
-    outDir: 'public/build',
-    manifest: true
-}
-});
+        outDir: 'public/build',
+        manifest: true,
+        emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                manualChunks: undefined
+            }
+        }
+    }
+})
